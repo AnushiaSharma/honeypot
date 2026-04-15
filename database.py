dbcc7f47c1c838044d0d721c496dbab7a203921e
Anushiasharma <sharmaanushia37@gmail.com>
2026-04-15 18:20:21 +0530

    Enhance honeypot functionality with admin login, session management, and improved dashboard UI

--- database.py
+++ database.py
@@ -17,14 +17,20 @@ def init_db():
     conn.commit()
     conn.close()
 
+from datetime import datetime
+import sqlite3
+
 def insert_attack(ip, username, password):
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
 
+    
+    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
     cursor.execute('''
-    INSERT INTO attacks (ip, username, password)
-    VALUES (?, ?, ?)
-    ''', (ip, username, password))
+    INSERT INTO attacks (ip, username, password, time)
+    VALUES (?, ?, ?, ?)
+    ''', (ip, username, password, time))
 
     conn.commit()
     conn.close()
--- web_honeypot.py
+++ web_honeypot.py
@@ -1,9 +1,16 @@
 import sqlite3
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, session
 from database import insert_attack, init_db
+from datetime import timedelta
+from flask import make_response
 
 app = Flask(__name__)
 
+app.config['SESSION_COOKIE_HTTPONLY'] = True
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
+
+app.secret_key = 'supersecretkey'
+app.permanent_session_lifetime = timedelta(minutes=5)
 init_db()
 
 @app.route('/', methods=['GET', 'POST'])
@@ -19,8 +26,28 @@ def index():
     
     return render_template('login.html')
 
+@app.route('/admin', methods=['GET', 'POST'])
+def admin_login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username == "admin" and password == "admin123":
+            session.permanent = True
+            session['admin'] = True
+            return redirect('/dashboard')
+        else:
+            return "Access Denied"
+        
+    return render_template('admin_login.html')
+
+    
+
 @app.route('/dashboard')
 def dashboard():
+    if not session.get('admin'):
+        return redirect('/admin')
+
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
 
@@ -29,7 +56,16 @@ def dashboard():
 
     conn.close()
 
-    return render_template('dashboard.html', data=rows)
+    response = make_response(render_template('dashboard.html', data=rows))
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
+
+@app.route('/logout')
+def logout():
+    session.pop('admin', None)
+    return redirect('/admin')
 
 def start_web():
     app.run(host='0.0.0.0', port=8080)
